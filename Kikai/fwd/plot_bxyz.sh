107c07cfea3b07ecc5bb022cc2561f26cc0412bb
takuto0126 <takuto.minami126@gmail.com>
2026-07-28 12:38:35 +0900

    further correction in plot_bxyz.sh

--- Kikai/fwd/plot_bxyz.sh
+++ Kikai/fwd/plot_bxyz.sh
@@ -13,7 +13,7 @@ gmt surface $topo -R$wesn -I1/1 -Gtopo.grd
 gmt makecpt -Cjet -T-1/5/0.01 
 
 #[bx] East
-gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50+l"Northing[km]" -BWeSn+t"|B_east| [nT]" -Y16
+gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50+l"Northing[km]" -BWeSn+t"|B east| [nT]" -Y16
 awk '{print($1,$2,$4)}' $infile | gmt surface -R$wesn -I$di -Ggrd.grd -T0
 gmt grdimage  grd.grd -C -R$wesn
 gmt grdcontour topo.grd -C50 -L-40/40 -W1
@@ -36,19 +36,19 @@ gmt colorbar -Dx11/0+w10/0.3 -B1+l"Amp nT" -G0/5
 gmt makecpt -Ccyclic -T-180/180/0.01 
 
 #[bz] East
-gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50+l"Northing[km]" -BWeSn+t"arg(B east) [nT]" -X-22 -Y-13
+gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50+l"Northing[km]" -BWeSn+t"arg(B east) [deg]" -X-22 -Y-13
 awk '{print($1,$2,$5)}' $infile | gmt surface -R$wesn -I$di -Ggrd.grd -T0
 gmt grdimage  grd.grd -C -R$wesn
 gmt grdcontour topo.grd -C50 -L-40/40 -W1
 
 #[bx] North
-gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50 -BWeSn+t"arg(B north) [nT]"  -X11
+gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50 -BWeSn+t"arg(B north) [deg]"  -X11
 awk '{print($1,$2,$7)}' $infile | gmt surface -R$wesn -I$di -Ggrd.grd -T0
 gmt grdimage  grd.grd -C -R$wesn
 gmt grdcontour topo.grd -C50 -L-40/40 -W1
 
 #[by] UP
-gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50 -BWeSn+t"arg(B up) [nT]" -X11
+gmt basemap -R$wesn -JX10/10 -Bxa50+l"Easting[km]" -Bya50 -BWeSn+t"arg(B up) [deg]" -X11
 awk '{print($1,$2,$9)}' $infile | gmt surface -R$wesn -I$di -Ggrd.grd -T0
 gmt grdimage  grd.grd -C -R$wesn
 gmt grdcontour topo.grd -C50 -L-40/40 -W1
